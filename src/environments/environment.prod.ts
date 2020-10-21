675bf12d3f44ae5322f0b7cc7ec93a72e3335f4d
Shubham More <shubhammore1796@gmail.com>
2020-10-22 00:30:56 +0530

    class-master

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,6 +9,6 @@ export const environment = {
   encKey: 'b417cf7714f5c803ac31ea7ea4ee3a11',
   aesKey: 'b417cf7714f5c803ac31ea7ea4ee3a11',
   razorpayKeyId: 'rzp_test_1JFxmyJ8eg8wQZ',
-  zoomLiveURL: 'http://35.154.130.138:3100/online-meeting',
+  zoomLiveURL: 'http://35.154.130.138:3100/online-meeting/#/',
   SOCKET_ENDPOINT: '/classMasterSocketConnection',
 };
