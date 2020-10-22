6a171f4158b15a52fafa3b5cd5fd4bb2154c4d3f
Shubham More <shubhammore1796@gmail.com>
2020-10-22 12:01:54 +0530

    class-master

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,6 +14,6 @@ export const environment = {
   encKey: 'b417cf7714f5c803ac31ea7ea4ee3a11',
   aesKey: 'b417cf7714f5c803ac31ea7ea4ee3a11',
   razorpayKeyId: 'rzp_test_1JFxmyJ8eg8wQZ',
-  zoomLiveURL: 'http://localhost:4000',
+  zoomLiveURL: 'http://localhost:4000/#/live',
   SOCKET_ENDPOINT: 'http://localhost:3000/classMasterSocketConnection',
 };
