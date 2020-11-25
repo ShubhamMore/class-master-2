9b40bed642428597ee2a3ba78fecf9e2c9c7e6ab
Shubham More <shubhammore1796@gmail.com>
2020-11-26 00:15:47 +0530

    sportizen

--- src/app/content/home/home.module.ts
+++ src/app/content/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NbCardModule, NbIconModule } from '@nebular/theme';
+import { NbCardModule, NbIconModule, NbActionsModule } from '@nebular/theme';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,13 @@ import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, CarouselModule, NbCardModule, NbIconModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    CarouselModule,
+    NbCardModule,
+    NbIconModule,
+    NbActionsModule,
+  ],
 })
 export class HomeModule {}
